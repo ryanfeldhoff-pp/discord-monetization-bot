dd7f074da5691a4e04524918f6d06337f10cf333
Feldy <ryan.feldhoff@prizepicks.com>
2026-04-01 13:29:26 -0600

    Add config/constants.py

---
+++ config/constants.py
@@ -0,0 +1,17 @@
+"""Application constants."""
+
+# Bot
+BOT_NAME = "Discord Monetization Bot"
+BOT_VERSION = "1.0.0"
+
+# Limits
+MAX_BALANCE = 1000000
+MIN_TRANSFER = 1
+MAX_TRANSFER = 100000
+
+# Database
+DEFAULT_DB_PATH = "data/bot.db"
+
+# Logging
+LOG_DIR = "logs"
+LOG_LEVEL = "INFO"
