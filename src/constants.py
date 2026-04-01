c3c2f8303bf0fd22a7ffb8d04006489f96992963
Feldy <ryan.feldhoff@prizepicks.com>
2026-04-01 13:29:46 -0600

    Add src/constants.py

---
+++ src/constants.py
@@ -0,0 +1,18 @@
+"""Global constants."""
+
+BOT_NAME = "Discord Monetization Bot"
+BOT_VERSION = "1.0.0"
+BOT_AUTHOR = "Ryan Feldhoff"
+
+# Discord
+DISCORD_API_VERSION = 10
+
+# Limits
+MIN_COINS = 0
+MAX_COINS = 10000000
+MIN_TRANSFER = 1
+MAX_TRANSFER = 100000
+
+# Timeouts
+COMMAND_TIMEOUT = 30
+API_TIMEOUT = 10
